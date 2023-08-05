2979cec822750e6a470f25644ddc69e39c761d07
IrynaKazmirchuk <irakazm@gmail.com>
2023-08-05 11:25:26 +0300

    ini CloudBudget

---
+++ script.js
@@ -0,0 +1,7 @@
+function navToggle() {
+	document.getElementById("headerUl").classList.toggle("_active");
+}
+
+function TogglePlayPause() {
+	document.getElementById("video").classList.toggle("_active");
+  }
