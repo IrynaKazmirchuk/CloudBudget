7ecf575333bebd4e86c0fc3d8f903cc102d18d91
IrynaKazmirchuk <irakazm@gmail.com>
2023-08-06 16:40:44 +0300

    init scroll and play

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ function navToggle() {
 	document.getElementById("headerUl").classList.toggle("_active");
 }
 
-function TogglePlayPause() {
-	document.getElementById("video").classList.toggle("_active");
-  }
+  $(function () {
+	$(".button").click(function () {
+	  $(".button").toggleClass("active");  
+		  $(".play").toggleClass("active");    
+	  $(".small-box").toggleClass("active");
+	  if($(".small-box").hasClass("active")) {
+		$(".small-box").removeClass("pauseToPlay");
+	  } else {
+		$(".small-box").addClass("pauseToPlay");
+	  }
+	  // $(input).addClass("active");
+	});
+  });
+  
